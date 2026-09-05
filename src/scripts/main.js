a4bc254683178101a2eb1bee0b8a4c5def6800da
AlexsanderGS <alexsanderga@gmail.com>
2026-09-05 16:34:06 -0300

    Adicionando funcionalidade ao botão de criar treino, ai clicar, abre o formulário para preenchimento.

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -3,3 +3,12 @@ import { initExercise } from "./exercise.js";
 
 initTraining();
 initExercise();
+
+const createButton = document.querySelector(".btn-create");
+const emptyState = document.querySelector(".empty-state");
+const trainingForm = document.querySelector(".training-form");
+
+createButton.addEventListener("click", () => {
+  emptyState.hidden = true;
+  trainingForm.hidden = false;
+});
