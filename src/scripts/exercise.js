7b3e88eee95bf1f4e274ff64ea6a1207389a1c3c
AlexsanderGS <alexsanderga@gmail.com>
2026-09-06 12:17:15 -0300

    Adicionando as funcionalidades ao formulario: Clonando os campos do formulario.

--- src/scripts/exercise.js
+++ src/scripts/exercise.js
@@ -1,1 +1,18 @@
-export function initExercise() {}
+export function initExercise() {
+  const addExerciseButton = document.querySelector(".bt-add-exercise");
+  const exerciseList = document.querySelector(".exercise-list");
+  const firstExerciseForm = document.querySelector(".exercise-form");
+
+  addExerciseButton.addEventListener("click", () => {
+    const exerciseForm = document.createElement("div");
+    exerciseForm.classList.add("exercise-form");
+
+    const newExerciseForm = firstExerciseForm.cloneNode(true);
+
+    exerciseList.appendChild(exerciseForm);
+    exerciseList.appendChild(newExerciseForm);
+  });
+
+  console.log(addExerciseButton);
+  console.log(exerciseList);
+}
--- src/scripts/training.js
+++ src/scripts/training.js
@@ -1,1 +1,9 @@
-export function initTraining() {}
+export function initTraining() {
+  const form = document.querySelector(".training-form form");
+
+  form.addEventListener("submit", (event) => {
+    event.preventDefault();
+
+    console.log("Formulário enviado!");
+  });
+}
